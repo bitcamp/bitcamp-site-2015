c6995319e884de856ac6236730b36d2110335022
Kunal <kunals1994@gmail.com>
2015-11-13 17:12:58 -0500

    adding javscript file for canvas

---
+++ js/sleeper.js
@@ -0,0 +1,12 @@
+// Copyright (c) 2015, Bitcamp
+
+function draw(){
+  var canvas = document.getElementById('sky-canvas');
+  var context = canvas.getContext();
+
+  context.font = "30px Arial";
+  context.fillStyle = "#FFFFFF";
+  context.fillText(".",100,100);
+
+  
+}
